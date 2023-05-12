897364671b601354dd3bdd30984a0b3141125afd
ijcrawford <ijcrawford@quinnipiac.edu>
2023-05-12 12:22:59 -0400

    filename fixing?

--- backend/models/courses.js
+++ backend/models/courses.js
@@ -5,7 +5,7 @@ var courseSchema = new Schema(
   {
     name: {type: String, required: true,unique: true},
     title: {type: String, required: true},
-    assignmentsId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'assignments', required: false}],
+    assignmentsId: [{ type: [mongoose.Schema.Types.ObjectId], ref: 'assignments', required: false}],
     studentsId: {type: [String], required: false},
     professorId: {type: String, required: true},
   },
